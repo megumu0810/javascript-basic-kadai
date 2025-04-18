289ceb9deef1f495c993a83617f1eccca860ad1d
megumu0810 <ikikmgmg0810@gmail.com>
2025-04-18 12:59:51 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -3,6 +3,5 @@ const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
     const childList = document.createElement('li');
-    childList.textContent = 'ボタンをクリックしました';
-    text.appendChild(childList);
+    text.textContent = 'ボタンをクリックしました';
 });
